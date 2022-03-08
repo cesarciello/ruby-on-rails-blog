3f307d0dc7eda1a6914d27fd04e5865febb30d22
Cesar Ciello <cesarciello15@gmail.com>
2022-03-08 15:44:38 -0300

    fix and refactor code in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,19 +3,27 @@ class ArticlesController < ApplicationController
   include Paginable
   before_action :authenticate_user!, except: %i[index show]
   before_action :set_article, only: %i[edit update destroy]
-  before_action :set_categories, only: %i[new create edit update]
+  before_action :set_categories, only: %i[index new create edit update]
 
   def index
-    @categories = Category.sorted
+    @archives = Article.group_by_month(:created_at, format: "%B %Y", locale: :en).count
+
     if params[:category].present?
-      category_filter = @categories.select do |category|
-                          category.name == params[:category]
-                        end [0]
+      category_filter = @categories.find do |category|
+        category.name == params[:category]
+      end
+    end
+
+    if params[:month_year].present?
+      month_year = @archives.find do |archives|
+        archives[0] == params[:month_year]
+      end
+      month_year = month_year&.first
     end
 
     @highlights = Article.includes(:category, :user)
                          .filter_by_category(category_filter)
-                         .filter_by_archive(params[:month_year])
+                         .filter_by_archive(month_year)
                          .desc_order
                          .first(3)
 
@@ -24,11 +32,9 @@ class ArticlesController < ApplicationController
     @articles = Article.includes(:category, :user)
                        .without_highlights(highlights_ids)
                        .filter_by_category(category_filter)
-                       .filter_by_archive(params[:month_year])
+                       .filter_by_archive(month_year)
                        .desc_order
                        .page(current_page)
-
-    @archives = Article.group_by_month(:created_at, format: '%B %Y', locale: :en).count
   end
 
   def show
@@ -44,7 +50,7 @@ class ArticlesController < ApplicationController
     @article = current_user.articles.new(article_params)
 
     if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
+      redirect_to @article, notice: "Article was successfully created."
     else
       render :new
     end
@@ -56,7 +62,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      redirect_to @article, notice: 'Article was successfully updated.'
+      redirect_to @article, notice: "Article was successfully updated."
     else
       render :edit
     end
@@ -64,7 +70,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    redirect_to root_path, status: :see_other, notice: 'Article was successfully destroyed.'
+    redirect_to root_path, status: :see_other, notice: "Article was successfully destroyed."
   end
 
   private
